249b110828202ec84c434ddaa272fced2d8fdc3e
Charles Jadin <jadinc@oregonstate.edu>
2017-11-14 16:33:54 -0600

    Added main node.js file and started the login page
    -login page wasn't my territory, but what I was working on was so similar that I figured I might as well throw something out for a little head start on it.  Don't feel any obligation to use it.
    -Started a node file, which I adapted from an old 290 assignment. It includes express, handlebars, body parser, and configuration for a connection to the sql database (I called the database "group20", not sure what the actual name was going to be).
    ***Note:  I used process.argv[] variables for port (index 2), as well as ONID name (3) and password (4) for DB connection.  So we can should be able to test the page by typing, for example, "node main.js 9999 bagginsf 3535", where 9999 is the port number, bagginsf is the ONID user name for Frodo Baggins, and 3535 are the last 4 digits of Frodo's student ID number.

---
+++ main.js
@@ -0,0 +1,75 @@
+/* 
+ * node.js file for CS361 Group 20 project.  This file requires 3 command line
+ * arguments after the filename:  port, ONID, and password.
+ */
+
+var express = require('express');
+
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'classmysql.engr.oregonstate.edu',
+  user            : 'cs361_' + process.argv[3],
+  password        : process.argv[4],
+  database        : 'group20'
+});
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.argv[2]);
+
+app.use(express.static('public'));
+
+//Render login page
+app.get('/', function(req, res, next){
+  var context = {};
+  res.render('login', context);
+});
+
+//Render main page
+app.get('/home', function(req, res, next){
+  var context = {};
+  res.render('home', context);
+});
+
+//Render login failure page
+app.get('/invalidlogin', function(req, res, next){
+  var context = {};
+  res.render('loginfailure', context);
+});
+
+//Render new user entry page
+app.get('/newuser', function(req, res, next){
+  var context = {};
+  res.render('newuser', context);
+});
+
+//Render edit account details page
+app.get('/editaccount',function(req, res, next){
+  var context = {};
+  res.render('editaccount', context);
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://flip2.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
