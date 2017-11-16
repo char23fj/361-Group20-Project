80d7c728cfdf99b5194fc4c6c9b8df074124323c
Charles Jadin <jadinc@oregonstate.edu>
2017-11-16 10:24:00 -0600

    Update main.js
    Uncommented 404/500 render calls after adding views for those cases.

--- main.js
+++ main.js
@@ -105,19 +105,18 @@ function loadPage(req, res, next) {
     res.render('login', context);
 }
 
-//Currently Breaking 
-/*
+
 app.use(function(req,res){
   res.status(404);
   res.render('404');
-});*/
+});
 
-/*app.use(function(err, req, res, next){
+app.use(function(err, req, res, next){
   console.error(err.stack);
   res.type('plain/text');
   res.status(500);
-  //res.render('500');
-});*/
+  res.render('500');
+});
 
 app.listen(app.get('port'), function(){
   console.log('Express started on http://flip2.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
