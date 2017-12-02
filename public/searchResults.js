bb5aa957f65a21a7fa9e641fe6ed602ef28c736a
Philip Sigillito <psigillito@gmail.com>
2017-12-01 22:37:44 -0600

    no results display

--- public/searchResults.js
+++ public/searchResults.js
@@ -2,6 +2,15 @@
 
 document.addEventListener("DOMContentLoaded", function(){
 
+    var count = document.getElementById("resultsTable").rows.length;
+    
+    if(count < 2){
+        document.getElementById("errorDiv").innerHTML = "ERROR";
+    
+    
+    }
 
 });
+
+
 
