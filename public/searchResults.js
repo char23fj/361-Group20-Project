116b08d517b56b1e9804f3d3814ef21b77694fc3
Philip Sigillito <psigillito@gmail.com>
2017-12-01 22:38:20 -0600

    no results display

--- public/searchResults.js
+++ public/searchResults.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(){
     var count = document.getElementById("resultsTable").rows.length;
     
     if(count < 2){
-        document.getElementById("errorDiv").innerHTML = "ERROR";
+        document.getElementById("errorDiv").innerHTML = "Sorry, There are no results that match your search";
     
     
     }
